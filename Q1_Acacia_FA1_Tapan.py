fcda62c262f7ee431abacc61bf4eb7540227c342
EcT21 <ecltapan31@src.pshs.edu.ph>
2026-08-11 09:25:34 +0800

    Update Q1_Acacia_FA1_Tapan.py

--- Q1_Acacia_FA1_Tapan.py
+++ Q1_Acacia_FA1_Tapan.py
@@ -10,7 +10,7 @@ y2 = float(input("Enter y2: "))
 distance = math.sqrt(math.pow(x2-x1,2) + math.pow(y2-y1,2))
 
 
-#answer
+#Output
 print(f"\n The distance between the two points is :{distance:.2f}")
 
 #REFLECTION :  Making a library is more practical because it provides ready made tested functions that save time and reduce errors in coding, for example, our distance calculator activity, using the math library's sqrt() and pow() functions made it easy to calculate the distance between two points without writing those calculations from scratch, making the program simpler and more reliable.
